ad65f63ed78c7f28af32f18d8333664760fed60e
Kopil Deb Roy Kabbo <kopildebroykabbo@gmail.com>
2025-08-16 22:10:44 +0600

    Update average_functions.py

--- Beginning codes/average_functions.py
+++ Beginning codes/average_functions.py
@@ -1,9 +1,13 @@
 def average_function():
+
+    n=int(input("Enter how many numbers you have:")
+    
     a=int(input("Enter your number:"))
     c=int(input("Enter your number:"))
     b=int(input("Enter your number:"))
-    average=(a+b+c)/3
+    
+    average=(a+b+c)/n
     print (average)
  
 average_function()  
-average_function() 
+ 
