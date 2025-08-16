e4ffccfd64f3dca42ee779be3375f00917c0795e
Kopil Deb Roy Kabbo <kopildebroykabbo@gmail.com>
2025-08-16 22:30:16 +0600

    Update average_functions.py
    Comments are added for your understanding.

--- Beginning codes/average_functions.py
+++ Beginning codes/average_functions.py
@@ -1,12 +1,12 @@
-def average_function():
+def average_function():   #starts a Function.
 
-    n=int(input("Enter how many numbers you have: "))
-    total=0
-    for i in range(n):
-        num=int(input(f"Enter number{i+1}: "))
-        total += num        
-    average=total/n
-    print (f"average is {average}")
+    n=int(input("Enter how many numbers you have: "))    #takes input of how many numbers, from users.
+    total=0 #initial value of sum is 0
+    for i in range(n): #starts a for loop
+        num=int(input(f"Enter number{i+1}: ")) #takes input of numbers from users
+        total += num  #on every loop sum the value of total and num and save it into total.       
+    average=total/n   #divides total by n
+    print (f"average is {average}") #prints the average value
  
-average_function()  
+average_function()  #call the Function 
  
