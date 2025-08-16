f1cfa5ec2dcd66938b79295f4031228f3b810037
Kopil Deb Roy Kabbo <kopildebroykabbo@gmail.com>
2025-08-16 22:08:24 +0600

    Average of 3 numbers

---
+++ Beginning codes/average_functions.py
@@ -0,0 +1,9 @@
+def average_function():
+    a=int(input("Enter your number:"))
+    c=int(input("Enter your number:"))
+    b=int(input("Enter your number:"))
+    average=(a+b+c)/3
+    print (average)
+ 
+average_function()  
+average_function() 
